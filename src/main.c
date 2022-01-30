b83f9a03d80edc764f55bbf9f1a7e0ed98239153
Alex Meier <ameier42@gmail.com>
2022-01-29 23:51:55 -0500

    finished initial stage of buffer

--- src/main.c
+++ src/main.c
@@ -11,8 +11,12 @@ int main(int argc, char** argv){
 	// get input from keyboard in a loop
 	while(1 == 1){
 		input_char = getchar();
-		buffer_put_char_to_curser(pBuffer, input_char);
-		printf("%s\n", pBuffer->contents);
+
+		if(input_char != '\n')
+		{
+			buffer_put_char_to_curser(pBuffer, input_char);
+			printf("%s\n", pBuffer->contents);
+		}
 	}
 
 	return 0; 
